31dbd02cc208b6a22e06f4891e0b2b789456e215
rxiua <rxiiua@gmail.com>
2025-03-14 16:31:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,36 +33,6 @@
 			alignment: 'right'
 		});
 
-	// NavPanel.
-
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
 	// Header.
 		if (!browser.mobile
 		&&	$header.hasClass('alt')
